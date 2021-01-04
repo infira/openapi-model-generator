6e7402e6a09a883a1f3fbdd6817503655980d8d8
Gen Taliaru <gen@infira.ee>
2021-01-04 19:58:17 +0200

    db2beta

--- inc/header.sh
+++ inc/header.sh
@@ -14,46 +14,55 @@ if [[ ! " ${domains[*]} " == *" $domain "* ]]; then
 fi
 
 declare -A domainLiveDb
+declare -A domainBetaDb
 declare -A domainLocalDb
 declare -A domainFullName
 declare -A localDomainName
 
 domainLiveDb[garmin]=d79590_lvgrm
+domainBetaDb[garmin]=d79590_betgrm
 domainLocalDb[garmin]=gws_garmin
 domainFullName[garmin]=garmineesti
 localDomainName[garmin]=garmin
 
 domainLiveDb[gopro]=d79590_lvgpr
+#domainBetaDb[gopro]=d79590_lvgpr
 domainLocalDb[gopro]=gws_gopro
 domainFullName[gopro]=prokaamera
 localDomainName[gopro]=gopro
 
 domainLiveDb[meremaailm]=d79590_lvmm
+#domainBetaDb[meremaailm]=d79590_lvmm
 domainLocalDb[meremaailm]=gws_meremaailm
 domainFullName[meremaailm]=meremaailm
 localDomainName[meremaailm]=meremaailm
 
 domainLiveDb[nutistuudio]=d79590_lvnut
+#domainBetaDb[nutistuudio]=d79590_lvnut
 domainLocalDb[nutistuudio]=gws_nutistuudio
 domainFullName[nutistuudio]=nutistuudio
 localDomainName[nutistuudio]=nutistuudio
 
 domainLiveDb[gps24]=d79590_lvgps24
+#domainBetaDb[gps24]=d79590_lvgps24
 domainLocalDb[gps24]=gws_gps24
 domainFullName[gps24]=gps24
 localDomainName[gps24]=gps24
 
 domainLiveDb[gpseesti]=d79590_lvgpe
+#domainBetaDb[gpseesti]=d79590_lvgpe
 domainLocalDb[gpseesti]=gws_gpseesti
 domainFullName[gpseesti]=gpseesti
 localDomainName[gpseesti]=gpseesti
 
 domainLiveDb[oakley]=d79590_lvgpr
+#domainBetaDb[oakley]=d79590_lvgpr
 domainLocalDb[oakley]=gws_oakley
 domainFullName[oakley]=oakstore
 localDomainName[oakley]=oakley
 
 domainLiveDb[intra]=d79590_livint
+domainBetaDb[intra]=d79590_intbeta
 domainLocalDb[intra]=gws_intra
 domainFullName[intra]=intra
 localDomainName[intra]=intra
