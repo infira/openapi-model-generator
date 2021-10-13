d8fd5150263aacfb582e80852f9ca328df5f4df0
Gen Taliar <gen@infira.ee>
2021-10-13 13:31:38 +0300

    gws databases conf

--- src/console/gws/Db.php
+++ src/console/gws/Db.php
@@ -25,41 +25,17 @@ class Db extends \Infira\Klahvik\console\Db
 		$fromLocal = $this->input->getOption('local');
 		
 		$databases = [
-			'garmin' => 'd79590_lvgrm',
-			'intra'  => 'd79590_livint',
+			'garmin'      => 'd79590_lvgrm',
+			'intra'       => 'd79590_livint',
+			'gopro'       => 'd79590_lvgpr',
+			'gps24'       => 'd79590_lvgps24',
+			'gpseesti'    => 'd79590_lvgpe',
+			'luxify'      => 'd79590_luxify',
+			'meremaailm'  => 'd79590_lvmm',
+			'miiego'      => 'd79590_miiego',
+			'nutistuudio' => 'd79590_lvnut',
+			'oakley'      => 'd79590_oakley',
 		];
-		
-		/*
-		domainLiveDb[intra]=d79590_livint
-		domainLocalDb[intra]=gws_NAME_intra
-		
-		domainLiveDb[garmin]=d79590_lvgrm
-		domainLocalDb[garmin]=gws_NAME_garmin
-		
-		domainLiveDb[gopro]=d79590_lvgpr
-		domainLocalDb[gopro]=gws_NAME_gopro
-		
-		domainLiveDb[gps24]=d79590_lvgps24
-		domainLocalDb[gps24]=gws_NAME_gps24
-		
-		domainLiveDb[gpseesti]=d79590_luxify
-		domainLocalDb[gpseesti]=gws_NAME_luxify
-		
-		domainLiveDb[gpseesti]=d79590_lvgpe
-		domainLocalDb[gpseesti]=gws_NAME_gpseesti
-		
-		domainLiveDb[meremaailm]=d79590_lvmm
-		domainLocalDb[meremaailm]=gws_NAME_meremaailm
-		
-		domainLiveDb[miiego]=d79590_miiego
-		domainLocalDb[miiego]=gws_NAME_miiego
-		
-		domainLiveDb[nutistuudio]=d79590_lvnut
-		domainLocalDb[nutistuudio]=gws_NAME_nutistuudio
-		
-		domainLiveDb[oakley]=d79590_oakley
-		domainLocalDb[oakley]=gws_NAME_oakley
-		 */
 		
 		if (!isset($databases[$domain]))
 		{
